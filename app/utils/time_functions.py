a43160e068622295002dd4cd3ee528730d927cbd
Shrinidhi N Hegde <shrinidhinhegde@gmail.com>
2023-04-12 11:53:29 +0530

    refactor: fixed wrong comment

--- app/utils/time_functions.py
+++ app/utils/time_functions.py
@@ -6,6 +6,6 @@ import datetime
 
 def current_utc_time():
     """
-    Returns current utc epoch
+    Returns current utc
     """
     return f'{datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]}Z'
